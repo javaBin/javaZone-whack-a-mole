08940c073723247dd09af931c94498fb14089e98
Rustam Mehmandarov <mehmandarov@gmail.com>
2017-09-12 00:40:38 +0200

    Added TODOs for the remaining work on the frontend logic

--- src/raspberry/app.py
+++ src/raspberry/app.py
@@ -5,6 +5,9 @@ from flask import Flask, url_for, request, render_template
 
 app = Flask(__name__, static_url_path='')
 
+#############
+# Web Endpoints
+#############
 @app.route('/')
 def welcome():
     return app.send_static_file('index.html')
@@ -13,15 +16,14 @@ def welcome():
 def game_on():
     return app.send_static_file('gameon.html')
 
-@app.route("/gameover")
-def get_data():
-	return str(json.load(urllib2.urlopen("http://harmannenfaltned.no/api")))
-
 @app.route('/showscore/')
-def hello(score=100000):
+def show_score(score=100000):
+	# TODO:  end game and get score from serial via API call – /api/endgame
     return render_template('showscore.html', score=score)
 
-
+#############
+# Internal API Endpoints
+#############
 @app.route("/api/startgame")
 def start_game():
 	# TODO: serial call to arduino to start the game
@@ -29,8 +31,7 @@ def start_game():
 
 @app.route("/api/endgame")
 def get_score():
-	# TODO: serial call to arduino to stop the game
-	# 
+	# TODO: serial call to arduino to stop the game and receive the score
     return "Score = "  
 
 def has_no_empty_params(rule):
@@ -38,6 +39,7 @@ def has_no_empty_params(rule):
     arguments = rule.arguments if rule.arguments is not None else ()
     return len(defaults) >= len(arguments)
 
+# List all available API Endpoints
 @app.route("/api")
 def site_map():
     links = []
