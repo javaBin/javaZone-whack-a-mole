059bc3622b2cc02caca0f1dd9e73104327067b16
Rustam Mehmandarov <mehmandarov@gmail.com>
2017-09-11 19:28:45 +0200

    First commit for the Python app to start and stop game

---
+++ src/raspberry/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    return "Go to /api"
+
+@app.route("/api/startgame")
+def start_game():
+    return "Starting Game"
+
+@app.route("/api/getscore")
+def get_score():
+    return "Score"  
+
+
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+@app.route("/api")
+def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    # links is now a list of url, endpoint tuples
+    return str(links)
+
+
+if __name__ == "__main__":
+    app.run()
